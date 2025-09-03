e433bbffeaa47c5b0bd1e73a44dff7544bf93733
tiltdepressed <vladisaev600@mail.ru>
2025-09-04 00:01:56 +0300

    13

---
+++ 13/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func swap(a, b *int) {
+	*a = *a + *b
+	*b = *b - *a
+	*b = -*b
+	*a = *a - *b
+}
+
+func main() {
+	a := 5
+	b := 13
+
+	fmt.Printf("old a: %d, old b: %d\n", a, b)
+	fmt.Println("SWAP!")
+	swap(&a, &b)
+	fmt.Printf("new a: %d, new b: %d\n", a, b)
+}
