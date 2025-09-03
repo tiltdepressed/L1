ff1e46caa82b461a9f96f5d05062cea609772b19
tiltdepressed <vladisaev600@mail.ru>
2025-09-03 20:09:54 +0300

    10

--- 10/main.go
+++ 10/main.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	arr := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
-	diapazoni := make(map[int][]float32)
+	arr := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	groups := make(map[int][]float64)
 
 	for _, val := range arr {
-		diapazoni[int(val)/10*10] = append(diapazoni[int(val)/10*10], val)
+		groups[int(val)/10*10] = append(groups[int(val)/10*10], val)
 	}
 
-	fmt.Println(diapazoni)
+	fmt.Println(groups)
 }
