47d2ec5ab3514325f348af8a169d730f9c1be6e4
tiltdepressed <vladisaev600@mail.ru>
2025-09-04 22:12:36 +0300

    17

--- 17/main.go
+++ 17/main.go
@@ -2,62 +2,48 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"math/rand/v2"
+	"slices"
 )
 
-func binSearch(arr []int, val, left, right int) int {
-	if left > right {
-		return -1
+func binarySearch(arr []int, target int) int {
+	left, right := 0, len(arr)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if arr[mid] == target {
+			return mid
+		}
+
+		if arr[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
 	}
 
-	mid := left + (right-left)/2
-
-	if arr[mid] == val {
-		return mid
-	} else if arr[mid] < val {
-		return binSearch(arr, val, mid+1, right)
-	} else {
-		return binSearch(arr, val, left, mid-1)
-	}
+	return -1
 }
 
 func main() {
 	arr := make([]int, 10)
-	for i := range len(arr) {
-		arr[i] = rand.Intn(20)
+	for i := range arr {
+		arr[i] = rand.IntN(20)
 	}
-	quickSort(arr, 0, len(arr)-1)
-	fmt.Println("Массив:", arr)
-	val := rand.Intn(20)
+
+	slices.Sort(arr)
+
+	fmt.Println("Отсортированный массив:", arr)
+
+	val := rand.IntN(20)
 	fmt.Println("Искомое число:", val)
-	index := binSearch(arr, val, 0, len(arr)-1)
-	fmt.Println("Индекс искомого элемента:", index)
-}
 
-func quickSort(arr []int, low, high int) {
-	i := low
-	j := high
-	pivot := arr[(i+j)/2]
+	index := binarySearch(arr, val)
 
-	for i <= j {
-		for arr[i] < pivot {
-			i++
-		}
-		for arr[j] > pivot {
-			j--
-		}
-		if i <= j {
-			temp := arr[i]
-			arr[i] = arr[j]
-			arr[j] = temp
-			i++
-			j--
-		}
-	}
-	if j > low {
-		quickSort(arr, low, j)
-	}
-	if i < high {
-		quickSort(arr, i, high)
+	if index != -1 {
+		fmt.Printf("Элемент %d найден по индексу: %d\n", val, index)
+	} else {
+		fmt.Printf("Элемент %d в массиве не найден.\n", val)
 	}
 }
