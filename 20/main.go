45b7573c44c6150a9864d03f729ae94f0bdb38b5
tiltdepressed <vladisaev600@mail.ru>
2025-09-05 00:48:37 +0300

    20

---
+++ 20/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func reverseRunes(r []rune, start, end int) {
+	for i, j := start, end; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+}
+
+func main() {
+	fmt.Print("Введите предложение: ")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		sentence := scanner.Text()
+		r := []rune(sentence)
+		reverseRunes(r, 0, len(r)-1)
+
+		wordStart := 0
+		for i := 0; i <= len(r); i++ {
+			if i == len(r) || r[i] == ' ' {
+				reverseRunes(r, wordStart, i-1)
+				wordStart = i + 1
+			}
+		}
+		fmt.Println("Развернутое предложение:", string(r))
+	}
+}
