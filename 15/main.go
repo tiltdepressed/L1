f11cba67c750971773e47ae8107acc948672dad4
tiltdepressed <vladisaev600@mail.ru>
2025-09-04 13:08:16 +0300

    15

---
+++ 15/main.go
@@ -0,0 +1,14 @@
+package main
+
+// var justString string
+//
+// func someFunc() {
+// 	v := createHugeString(1 << 10)
+// 	justString = v[:100]
+// }
+//
+// func main() {
+// 	someFunc()
+// }
+//
+// Проблема кода -
